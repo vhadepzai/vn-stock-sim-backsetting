19b0565ff15110cd4f2a34418b2af802a22e2f91
vhadepzai <buitrongvietha07@gmail.com>
2026-09-04 09:17:52 +0700

    Update app.py
    as requirements change, so main code should be changed too

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from vnstock import *
+from vnstock3 import Vnstock
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import random
